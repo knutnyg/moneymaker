82b2e429b24610ce49e6f1e621d3ed5b1e1adae6
Eivind Siqveland Larsen <eivind@siqve.land>
2021-11-20 14:17:10 +0100

    UI in browser

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -29,6 +29,9 @@ export interface AppState {
     lastUpdatedAt: Date
 }
 
+const H1: React.FC = ({children}) => <h1 style={{paddingTop: '10px'}}>{children}</h1>
+const H2: React.FC = ({children}) => <h2 style={{paddingTop: '10px'}}>{children}</h2>
+
 function AppStateView(props: { state: AppState | undefined }) {
     const state = props.state;
 
@@ -39,17 +42,51 @@ function AppStateView(props: { state: AppState | undefined }) {
         <div>
             <div>
                 <div>Last updated:</div>
-                <div>{state.lastUpdatedAt.toISOString()}</div>
+                <div>{getRelativeTime(state.lastUpdatedAt)}</div>
             </div>
+            <H2>Active orders</H2>
             <div>
-                <div>{state.activeTrades.activeOrders.map(a => <div>{JSON.stringify(a)}</div>)}</div>
+                <div
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: '2fr 1fr 1fr 1fr 2fr',
+                    }}
+                >
+                    {state.activeTrades.activeOrders.map(a => <Fragment>
+                        <div>{format(a.created_at)}</div>
+                        <div>{a.market}</div>
+                        <div>{a.amount}</div>
+                        <div>{a.price}</div>
+                        <div>{a.price * a.amount}</div>
+                    </Fragment>)}
+                </div>
             </div>
-            <h2>Trade History</h2>
+            <H2>Trade History</H2>
             <div>
-                <div>{state.filledOrders.filledOrders.map(a => <div>{JSON.stringify(a)}</div>)}</div>
+                <div
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: '2fr 1fr 1fr 1fr 2fr',
+                    }}
+                >
+                    {state.filledOrders.filledOrders.map(a => <Fragment>
+                        <div>{format(a.created_at)}</div>
+                        <div>{a.market}</div>
+                        <div>{a.amount}</div>
+                        <div>{a.price}</div>
+                        <div>{a.price * a.amount}</div>
+                    </Fragment>)}
+                </div>
             </div>
         </div>
     );
+}
+
+function format(d: Date): string {
+    const parts = d.toISOString().split('T');
+    const date = parts[0]
+    const time = parts[1].substr(0, 8)
+    return `${date} ${time}`
 }
 
 const DataSource: React.FC = () => {
@@ -70,6 +107,12 @@ const DataSource: React.FC = () => {
                     lastUpdatedAt: new Date(nextState.activeTrades.lastUpdatedAt * 1000),
                 }
                 nextState.activeTrades.activeOrders = nextState.activeTrades.activeOrders.map((o: { created_at: number; }) => {
+                    return {
+                        ...o,
+                        created_at: new Date(o.created_at * 1000),
+                    }
+                })
+                nextState.filledOrders.filledOrders = nextState.filledOrders.filledOrders.map((o: { created_at: number; }) => {
                     return {
                         ...o,
                         created_at: new Date(o.created_at * 1000),
@@ -114,3 +157,29 @@ function App() {
 }
 
 export default App;
+
+// in miliseconds
+const units = {
+    year: 24 * 60 * 60 * 1000 * 365,
+    month: 24 * 60 * 60 * 1000 * 365 / 12,
+    day: 24 * 60 * 60 * 1000,
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+    second: 1000
+}
+
+const rtf = new Intl.RelativeTimeFormat('nb', {numeric: 'auto'})
+
+const getRelativeTime = (d1: Date, d2 = new Date()) => {
+    const elapsed = d1.valueOf() - d2.valueOf()
+
+    // "Math.abs" accounts for both "past" & "future" scenarios
+    for (var u in units) {
+        // @ts-ignore
+        if (Math.abs(elapsed) > units[u] || u === 'second') {
+            // @ts-ignore
+            return rtf.format(Math.round(elapsed / units[u]), u)
+        }
+    }
+}
+
