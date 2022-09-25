b5dea63013bdeb855333d1bd78592cadc15a9315
Eivind Siqveland Larsen <eivind@siqve.land>
2022-09-25 23:10:29 +0200

    bump frontend

--- src/main/frontend/src/app/Base.tsx
+++ src/main/frontend/src/app/Base.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const H1: React.FC = ({ children }) => (
+export const H1: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <h1 style={{ paddingTop: '10px' }}>{children}</h1>
 )
-export const H2: React.FC = ({ children }) => (
+export const H2: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <h2 style={{ paddingTop: '10px' }}>{children}</h2>
 )
--- start.sh
+++ start.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
+#nvm use --lts
+
 git pull
 cd src/main/frontend || exit
 npm run build
 cd ../../../
 ./gradlew clean jar
-java -jar build/libs/moneymaker-1.0.jar
+java -jar build/libs/moneymaker-1.0.jarjava -jar build/libs/moneymaker-1.0.jar
